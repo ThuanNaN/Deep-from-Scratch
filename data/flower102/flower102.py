a46c18a7ab48df0fdf8a6d1f4340db712f336011
ThuanNaN <duongthuan1445@gmail.com>
2023-12-11 23:48:22 +0700

    update clean code

--- data/flower102/flower102.py
+++
@@ -1,5 +0,0 @@
-import wget 
-
-
-flower101_link = "https://www.robots.ox.ac.uk/~vgg/data/flowers/102/102flowers.tgz"
-wget.download(flower101_link, out="./download/flower101.zip")
--- data/food101/food101.py
+++
@@ -1,9 +0,0 @@
-import wget 
-
-
-
-
-food101_link = "http://data.vision.ee.ethz.ch/cvl/food-101.tar.gz"
-wget.download(food101_link, out='./download/food101.zip')
-
-
